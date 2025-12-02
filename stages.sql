1128b1a68f2bac86a26bed3d0d1b05de5466ae6f
ayoub timoumi <ayoubtimoumi01@gmail.com>
2025-12-02 09:16:58 +0100

    added correct db

--- stages.sql
+++ stages.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1:3306
--- Généré le : mar. 02 déc. 2025 à 00:59
--- Version du serveur : 10.4.32-MariaDB
--- Version de PHP : 8.2.12
+-- Host: 127.0.0.1
+-- Generation Time: Dec 02, 2025 at 09:15 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,34 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `stages`
+-- Database: `stages`
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `enseignant`
---
-
-CREATE TABLE `enseignant` (
-  `CIN` int(11) NOT NULL,
-  `nom` varchar(10) NOT NULL,
-  `prenom` varchar(10) NOT NULL,
-  `user_id` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
-
---
--- Déchargement des données de la table `enseignant`
---
-
-INSERT INTO `enseignant` (`CIN`, `nom`, `prenom`, `user_id`) VALUES
-(785698, 'hamza', 'sameh', 18),
-(12421295, 'amira', 'jouini', 2);
-
--- --------------------------------------------------------
-
---
--- Structure de la table `etudiant`
+-- Table structure for table `etudiant`
 --
 
 CREATE TABLE `etudiant` (
@@ -57,89 +36,33 @@ CREATE TABLE `etudiant` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Déchargement des données de la table `etudiant`
+-- Dumping data for table `etudiant`
 --
 
 INSERT INTO `etudiant` (`NCE`, `nom`, `prenom`, `classe`, `user_id`) VALUES
-(123456, 'ouni', 'samar', 'dsi 202', 4),
-(236667, 'balkis', 'toujeni', 'rsi 302', 5),
+(123456, 'samour', 'smayra', 'dsi 202', 4),
 (479685, 'timoumi', 'youbou', 'dsi202', 20),
 (852365, 'bouassida', 'ghada', 'dsi 302', 7),
-(963258, 'srairi', 'siwar', 'rsi 201', 8);
-
--- --------------------------------------------------------
-
---
--- Structure de la table `user`
---
-
-CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
-  `login` varchar(10) NOT NULL,
-  `mot_de_passe` varchar(20) NOT NULL,
-  `role` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+(963258, 'srairi', 'siwar', 'rsi 201', 8),
+(12345678, 'ekhdem', 'aad', 'DSI508', 22);
 
 --
--- Déchargement des données de la table `user`
---
-
-INSERT INTO `user` (`id`, `login`, `mot_de_passe`, `role`) VALUES
-(1, 'admin', 'adminadmin', 'admin'),
-(2, 'amiraaa', 'aaa', 'enseignant'),
-(4, 'samaar', 'Samaar1234', 'etudiant'),
-(5, 'balkis', 'balkistoujeni', 'etudiant'),
-(7, 'ghada', 'ghadabouassida', 'etudiant'),
-(8, 'siwar', 'siwarsrairi', 'etudiant'),
-(18, 'hamza', 'hamzasameh', 'enseignant'),
-(20, 'timoumi', 'timoumiayoub', 'etudiant');
-
---
--- Index pour les tables déchargées
+-- Indexes for dumped tables
 --
 
 --
--- Index pour la table `enseignant`
---
-ALTER TABLE `enseignant`
-  ADD PRIMARY KEY (`CIN`),
-  ADD KEY `fk_enseignant_user` (`user_id`);
-
---
--- Index pour la table `etudiant`
+-- Indexes for table `etudiant`
 --
 ALTER TABLE `etudiant`
   ADD PRIMARY KEY (`NCE`),
   ADD KEY `fk_etudiant_user` (`user_id`);
 
 --
--- Index pour la table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT pour les tables déchargées
+-- Constraints for dumped tables
 --
 
 --
--- AUTO_INCREMENT pour la table `user`
---
-ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
-
---
--- Contraintes pour les tables déchargées
---
-
---
--- Contraintes pour la table `enseignant`
---
-ALTER TABLE `enseignant`
-  ADD CONSTRAINT `fk_enseignant_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
-
---
--- Contraintes pour la table `etudiant`
+-- Constraints for table `etudiant`
 --
 ALTER TABLE `etudiant`
   ADD CONSTRAINT `fk_etudiant_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
